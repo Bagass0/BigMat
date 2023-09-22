d54c8e8fce749402161d435b5ac58aedbded3c49
Hugo <80489889+Bagass0@users.noreply.github.com>
2023-09-22 16:50:03 +0200

    Header-Update

--- header.php
+++ header.php
@@ -6,10 +6,11 @@ require_once './connect_societe.php';
 		<div id="nav1" style="box-shadow: 0 0.5em 0.7em -0.5em rgba(0, 0, 0, 0.1);" class="bgColorHeader">
 			<a href="index.php" class="logo" style="width: auto;"></a>
 			<nav id="nav" style="background-color: rgba(0, 0, 0, 0.4);">
-				<div class="inner marginTop" style="max-width: 100%;">
-					<a style="padding: 0; margin: 0;" href="index.php"><img src="images/logo.png" style="width: 10em; margin: 0.5em" class="displayHeader2"></a>
-					<?php include 'menu.php'; ?>
-					<div class="inner displayHeader" style="display: flex; max-width: 100%; max-width: 100%;">
+				<div class="inner" style="max-width: 100%;">
+					<a href="index.php"><img src="images/logo.png" style="width: 10em; margin: 0.5em" class="displayHeader2">
+						<?php include 'menu.php'; ?>
+					</a>
+					<div class="Box_nav_user">
 						<?php include 'menu1.php'; ?>
 					</div>
 				</div>
